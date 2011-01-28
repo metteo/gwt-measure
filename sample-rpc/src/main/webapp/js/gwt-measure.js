602b9b7237801975a4464f505edb655c0a4419ce
buzdin <buzdin@localhost>
2011-01-28 17:05:19 +0200

    copyright added;

--- sample-rpc/src/main/webapp/js/gwt-measure.js
+++ sample-rpc/src/main/webapp/js/gwt-measure.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var gwt_initialized = false;
 
 function sinkGwtEvents() {
@@ -21,7 +37,7 @@ window.__gwtStatsEvent = function(event) {
     if (gwt_initialized) {
         //if flag is set to true, then all events goes to GWT created method
         sinkGwtEvents();
-        handleEvent("" + event.evtGroup, event.moduleName + "." + event.subSystem, "" + event.type, "" + event.millis);
+        handleEvent("" + event.evtGroup, "" + event.moduleName, "" + event.subSystem, "" + event.type, "" + event.millis);
     } else {
         //if flag is not set to true, then all events goes to eventStorage
         this.eventBuffer = (this.eventBuffer) ? this.eventBuffer : [];
